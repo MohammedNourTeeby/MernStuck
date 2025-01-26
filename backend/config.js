da481b81eab7bf2b8a78ec092b1e8881b465af62
mohammednourteeby <mohammednourteeby@gmail.com>
2025-01-26 20:47:51 +0200

    finall Full Stuck

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://mohamdnour:mhmdnourteeby@mernstuck.f7kju.mongodb.net/?retryWrites=true&w=majority&appName=mernstuck";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
